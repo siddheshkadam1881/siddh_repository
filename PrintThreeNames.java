f080ad84481c1b262ebccef1c097f967ef1004f3
siddheshkadam1881 <32275172+siddheshkadam1881@users.noreply.github.com>
2017-09-26 23:45:34 +0530

    Add files via upload

---
+++ PrintThreeNames.java
@@ -0,0 +1,12 @@
+class PrintThreeNames
+{
+ public static void main(String a[])
+{
+   String s1=a[0];
+   String s2=a[1];
+   String s3=a[2];
+
+    String s4="Hi "+a[2]+" ,"+a[1]+" ,"+a[0];
+    System.out.println(s4);
+}
+}
