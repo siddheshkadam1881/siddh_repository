015ec617efedb013e671ed91c8f133d697b02b51
siddheshkadam1881 <32275172+siddheshkadam1881@users.noreply.github.com>
2017-09-27 21:04:00 +0530

    Add files via upload

---
+++ PrintInitials.java
@@ -0,0 +1,16 @@
+public class PrintInitials 
+{
+    public static void main(String args[])
+{
+
+       System.out.println("**     ***    **********      **            *             **");
+       System.out.println("**    ***     **        **     **          ***           **");
+       System.out.println("**   ***      **         **     **        ** **         **");
+       System.out.println("**  ***       **          **     **      **   **       **");
+       System.out.println("*****         **          **      **    **     **     **");
+       System.out.println("**  ***       **         **        **  **       **   **");
+       System.out.println("**   ***      **        **          ** **        ** **");
+       System.out.println("**    ***     **       **            ***          ***");
+       System.out.println("**     ***    **********              *            *");
+    }
+}
