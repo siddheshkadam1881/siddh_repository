e4719e121a96d55e23bc973a488e4bc7a85c306d
siddheshkadam1881 <32275172+siddheshkadam1881@users.noreply.github.com>
2017-09-30 18:04:45 +0530

    Add files via upload

---
+++ CarLoan.java
@@ -0,0 +1,20 @@
+
+public class CarLoan
+ {
+    public static void main(String[] args)
+ {
+        double principal = Double.parseDouble(args[0]);
+        double years = Double.parseDouble(args[1]);
+        double rate = Double.parseDouble(args[2]);
+
+        double r = rate / 12 / 100; 
+        double n = 12 * years;      
+
+        double payment  = (principal * r) / (1 - Math.pow(1+r, -n));
+        double interest = payment * n - principal;
+
+        System.out.println("Monthly payments = " + payment);
+        System.out.println("Total interest   = " + interest);
+    }
+
+}
