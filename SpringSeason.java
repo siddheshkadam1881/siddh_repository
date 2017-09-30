dfcc30b6115ad994970f0c1ce1e6edde4111d9ba
siddheshkadam1881 <32275172+siddheshkadam1881@users.noreply.github.com>
2017-09-30 14:35:50 +0530

    Add files via upload

---
+++ SpringSeason.java
@@ -0,0 +1,14 @@
+  public class SpringSeason {
+
+      public static void main( String[] args) 
+       {
+         boolean isSpring=false;
+        int month=Integer.parseInt(args[0]);
+        int day=Integer.parseInt(args[1]);
+        
+         isSpring=((month==3 && day>=20 && day<=31) || ( month==4 && day>=1 && day<=30) || ( month ==5 && day>=1 && day<=31) || ( month==6 && day>=1 && day<=20));
+         {
+          System.out.println(isSpring);
+          }
+       }
+}
