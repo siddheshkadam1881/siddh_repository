df4f3ef51d585729e876463a1775bbfbc850d3cf
siddheshkadam1881 <32275172+siddheshkadam1881@users.noreply.github.com>
2017-09-30 13:20:12 +0530

    Add files via upload

---
+++ IntOpt.java
@@ -0,0 +1,21 @@
+class IntOpt
+{
+  public static void main(String args[])
+{
+  int a=Integer.parseInt(args[0]);
+  int b=Integer.parseInt(args[1]);
+  int c=Integer.parseInt(args[2]);
+    int d= a+(b*c);
+   System.out.println("calculation of 1:"+ d); 
+    int e=(a*b)+c;
+   System.out.println("calculation of 2:"+ e); 
+    int f=c+(a/b);
+   System.out.println("calculation of 3:"+ f);
+    int g=(a%b)+c;
+   System.out.println("calculation of 4:"+ g);
+      
+}
+}
+    
+
+      
